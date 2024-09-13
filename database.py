6f7e5ca4c8927659803dbfb965f5b978533a0e24
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-09-14 00:01:48 +0530

    image_saving

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:9922557gimna@localhost:3307/music_ai_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:12345@localhost:3306/music_ai_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
